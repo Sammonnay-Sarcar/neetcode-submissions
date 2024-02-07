30bbd72c5475dffc805d0b1af01b10a2f25a766f
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2024-02-07 23:19:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.6 MB (99.99%)

---
+++ 0035-search-insert-position/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length-1, mid ;
+        while(left<=right){
+            mid = left+(right - left)/2;
+            if(target>nums[mid]){
+                left = mid+1;
+            }else if(target<nums[mid]){
+                right = mid-1;
+            }else if(target == nums[mid]){
+                return mid;
+            }
+        }
+        return(left);
+    }
+}
