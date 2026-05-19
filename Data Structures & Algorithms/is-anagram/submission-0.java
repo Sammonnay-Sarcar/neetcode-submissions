039696a32d37c4d7e7e787c6ee170f822c5ca5e3
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-05-19 21:01:56 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] sArr = new int[26];
+        Arrays.fill(sArr,0);
+        
+        for(int i = 0; i<s.length();i++){
+            sArr[s.charAt(i)-'a']++;
+        }
+        
+        for(int i = 0; i<t.length();i++){
+            sArr[t.charAt(i)-'a']--;
+        }
+        for(int i = 0; i<26;i++){
+            if(sArr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
