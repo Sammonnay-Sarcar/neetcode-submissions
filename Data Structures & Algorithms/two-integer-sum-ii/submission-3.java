a164dad62d1cc8d5084ab7e253f7a652e2049a2e
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-05-29 13:22:42 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int leftPointer = 0;
+        int rightPointer = numbers.length -1;
+        while(leftPointer < rightPointer){
+            if(numbers[leftPointer]+numbers[rightPointer] < target){
+                leftPointer++;
+            }else if(numbers[leftPointer]+numbers[rightPointer] > target){
+                rightPointer--;
+            }else{
+                return new int[]{leftPointer+1,rightPointer+1};
+            }
+        }
+        return new int[]{0,0};
+    }
+}
