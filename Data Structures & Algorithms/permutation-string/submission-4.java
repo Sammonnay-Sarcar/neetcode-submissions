cfcf2841262eb4d3b1edf55134d279790e4a9798
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-06-04 21:13:44 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1Arr = new int[26];
+        int[] s2Arr = new int[26];
+        if(s1.length()>s2.length()){
+            return  false;
+        }
+        for(int i =0;i<s1.length();i++){
+            s1Arr[s1.charAt(i)-'a']++;       
+        }
+        for(int i =0;i<s1.length()-1;i++){
+            s2Arr[s2.charAt(i)-'a']++;       
+        }
+        int l = 0;
+        for(int r =s1.length()-1;r<s2.length();r++){   
+            s2Arr[s2.charAt(r)-'a']++; 
+            if(Arrays.equals(s1Arr,s2Arr)){
+                return true;
+            }
+            s2Arr[s2.charAt(l)-'a']--;
+            l++;
+        }
+        return false;
+    }
+}
