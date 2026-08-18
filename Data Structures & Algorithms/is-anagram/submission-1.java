e70c643c2c55466d113dd4b4d20cfb3acd1372e9
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-08-18 22:03:47 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr1 = new int [26];
+        int[] arr2 = new int [26];
+        for(char ch : s.toCharArray()){
+            arr1[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            arr2[ch-'a']++;
+        }
+        return Arrays.toString(arr1).equals(Arrays.toString(arr2));
+    }
+}
