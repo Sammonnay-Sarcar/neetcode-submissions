c1232e0ad8d67d72ce6e954e720650314e5fe1c9
Sammonnay Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-08-24 17:30:35 +0530

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int[] s2Arr = new int[26];
+        int[] s1Arr = new int[26];
+        for(int i = 0; i <s1.length();i++){
+            s2Arr[s2.charAt(i)-'a']++;
+            s1Arr[s1.charAt(i)-'a']++;
+        }
+        for(int i = s1.length();i<s2.length();i++){
+            if(Arrays.equals(s1Arr, s2Arr)){
+                return true;
+            }
+            s2Arr[s2.charAt(i-s1.length())-'a']--;
+            s2Arr[s2.charAt(i)-'a']++;
+        }
+        return Arrays.equals(s1Arr, s2Arr);
+    }
+}
